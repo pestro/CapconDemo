1b9ae97e2bb576c03e40c012fd12a0e754d618b1
pestro <p3rstr4nd@gmail.com>
2019-11-24 22:08:08 -0500

    Making conversions more modular

--- src/main/scala/Mapper.scala
+++ src/main/scala/Mapper.scala
@@ -1,11 +1,23 @@
 import shapeless.{::, HList, HNil, Poly1, _}
+import Data._, Data.Id, Data.Processed._
 
 object Mapper {
 
-  object encoder extends Poly1 {
-    import Data._
-    import Data.Processed._
+  object convert {
+    trait name extends Poly1 {
+      implicit def caseName = at[Name] {
+        case Name(name) => s"$name has been converted!"
+      }
+    }
 
+    trait balance extends Poly1 {
+      implicit def caseBalance = at[Balance] {
+        case Balance(balance) => Balance(balance / 10)
+      }
+    }
+  }
+
+  object encoder extends Poly1 with convert.balance with convert.name {
     // Can either go from A -> A or...
     implicit def caseRank = at[Rank]{
       case Rank(code) => Rank(s"$code has been encoded!")
@@ -15,14 +27,8 @@ object Mapper {
     implicit def caseId = at[Id] {
       case Id(id) => EncodedId(s"$id has been encoded!")
     }
-
-    implicit def caseName = at[Name] {
-      case Name(name) => s"$name has been encoded!"
-    }
-
-    implicit def caseBalance = at[Balance] {
-      case Balance(balance) => Balance(balance / 10)
-    }
   }
 
+  // We can now use the granular conversions in order to setup more encompassing strategies
+  object convertAccountSummaries extends convert.balance with convert.name
 }
